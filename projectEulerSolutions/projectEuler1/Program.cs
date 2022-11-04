031dac7f9b2ec2c5e7ea17cc0ad3ceef03246302
Wynand Karsten <wynandkarsten@gmail.com>
2022-11-04 16:14:03 +0200

    Initial project files for Problem 1

---
+++ projectEulerSolutions/projectEuler1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace projectEuler1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = 0;
+            for (int i = 1; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    answer += i;
+                }
+            }
+            Console.WriteLine("The answer is: " + answer);
+            Console.ReadLine();
+        }
+    }
+}
